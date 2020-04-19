bdc38dd9081185bfbf002c457e464a4d369f9af7
Fatih Yıldırım <36747348+yldrimftih@users.noreply.github.com>
2020-04-19 14:49:44 +0300

    Toast kullanımı
    Toat kullanımı ve textviewden alınan textin toast içerisinde çekilmesi uygulandı. Sırada butonlar var.

--- ikinciornek/app/src/main/java/com/example/ikinciornek/MainActivity.java
+++ ikinciornek/app/src/main/java/com/example/ikinciornek/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.ikinciornek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Bu bir acil durum mesajıdır.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView kedicik = new ImageView(getApplicationContext());// getApplicationContext() yerine MainActivity.this yazsak da olur.
+                kedicik.setImageResource(R.drawable.kedi);
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(kedicik);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = new TextView(getApplicationContext());
+                textView.setText("Bu bir dramdır..");
+                textView.setBackgroundColor(Color.parseColor("#009688"));
+                textView.setTextColor(Color.WHITE);
+                textView.setPadding(30,10,10,10);
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(textView);
+                
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
     }
 }
---
+++ ucuncuornek/app/src/main/java/com/example/ucuncuornek/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ucuncuornek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
